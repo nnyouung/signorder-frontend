ecc6d212d6b21d96189f253ae20fa196c31bd7a5
decollozq <decollzoq@gmail.com>
2025-07-11 12:28:16 +0900

    feat: OxSelectionAnswerActivity 에서 버튼에 따른 화면 전환

--- counter_app/app/src/main/java/com/google/mediapipe/examples/handlandmarker/OxSelectionAnswerActivity.kt
+++ counter_app/app/src/main/java/com/google/mediapipe/examples/handlandmarker/OxSelectionAnswerActivity.kt
@@ -1,11 +1,37 @@
 package com.google.mediapipe.examples.handlandmarker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 
 class OxSelectionAnswerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ox_selection_answer)
+
+        val inquiryNumber = intent.getIntExtra("inquiry_number", -1)
+        println("전달받은 문의 번호: $inquiryNumber")
+
+        val backButton = findViewById<ImageButton>(R.id.backButton)
+        backButton.setOnClickListener{
+            onBackPressedDispatcher.onBackPressed()
+        }
+
+        // To-do : OX 버튼 이동 흐름
+        val yesButton = findViewById<ImageButton>(R.id.restRoomButton) // O 버튼 id로 바꿔야 함
+        val noButton = findViewById<ImageButton>(R.id.wifiButton)   // X 버튼 id로 바꿔야 함
+
+        yesButton.setOnClickListener{
+            val intent = Intent(this, QuestionActivity::class.java)
+            intent.putExtra("inquiry_number", inquiryNumber)
+            startActivity(intent)
+        }
+
+        noButton.setOnClickListener{
+            val intent = Intent(this, QuestionActivity::class.java)
+            finish()
+        }
     }
+
 }
