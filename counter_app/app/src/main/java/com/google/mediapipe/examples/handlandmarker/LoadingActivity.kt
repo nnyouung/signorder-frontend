94e0525535b271821b79b7f70a96a26fbffdb09e
decollozq <decollzoq@gmail.com>
2025-07-11 21:10:44 +0900

    feat: 로딩 화면에서 WebSocket 메세지 수신 및 AnswerActivity 전환 로직 구현

--- counter_app/app/src/main/java/com/google/mediapipe/examples/handlandmarker/LoadingActivity.kt
+++ counter_app/app/src/main/java/com/google/mediapipe/examples/handlandmarker/LoadingActivity.kt
@@ -1,11 +1,30 @@
 package com.google.mediapipe.examples.handlandmarker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 
 class LoadingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_loading)
+
+        val backButton = findViewById<ImageButton>(R.id.backButton)
+        backButton.setOnClickListener{
+            onBackPressedDispatcher.onBackPressed()
+        }
+
+        // sign_urls 수신 시 AnswerActivity로 전환
+        // LoadingActivity는 중간 대기 화면으로 활용
+        WebSocketService.onSignUrlsReceived = { urls ->
+            runOnUiThread {
+                val intent = Intent(this, AnswerActivity::class.java)
+                intent.putStringArrayListExtra("sign_urls", ArrayList(urls))
+                startActivity(intent)
+                finish()
+            }
+        }
+
     }
 }
