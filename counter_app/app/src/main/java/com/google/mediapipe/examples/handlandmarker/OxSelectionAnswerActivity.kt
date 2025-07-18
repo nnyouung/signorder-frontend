a98211bac3935b0763bf75e5e1efc9a1b90e46b2
nnyouung <211_0@kookmin.ac.kr>
2025-07-18 16:41:04 +0900

    feat: 예/아니오 버튼 UI 추가 및 gRPC 연결
    수정사항
    - OxSelectionAnswerActivity.kt: 아니오 버튼에 gprc 연결 (FastInquiry 호출)
    - activity_ox_selection_answer.xml, yes_no_button_backgound.xml: 예/아니오 버튼 추가

--- counter_app/app/src/main/java/com/google/mediapipe/examples/handlandmarker/OxSelectionAnswerActivity.kt
+++ counter_app/app/src/main/java/com/google/mediapipe/examples/handlandmarker/OxSelectionAnswerActivity.kt
@@ -4,16 +4,30 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
+import android.widget.LinearLayout
+import com.example.grpc.GrpcClient
 
 class OxSelectionAnswerActivity : AppCompatActivity() {
+    private val grpcClient = GrpcClient()
+    private lateinit var signVideo: SignVideo
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ox_selection_answer)
 
-        // TODO : "문의사항 있으신가요?" 수어 아바타 영상 띄우기
+        signVideo = findViewById(R.id.signVideo)
+
+        signVideo.setup(
+            listOf(
+                R.raw.video1_qna_additional,
+                R.raw.video2_guide_additional_qna,
+                R.raw.video3_guide_korean_signlang,
+                R.raw.video4_guide_signlang_complete
+            )
+        )
 
         val inquiryNumber = intent.getIntExtra("inquiry_number", -1)
-        val layoutType = intent.getStringExtra("layoutType")
+        val layoutType = intent.getStringExtra("layoutType")?: "inquiry"
         println("전달받은 문의 번호: $inquiryNumber")
 
         val backButton = findViewById<ImageButton>(R.id.backButton)
@@ -24,11 +38,10 @@ class OxSelectionAnswerActivity : AppCompatActivity() {
             finish()
         }
 
-        // TODO 1 : xml에서 OX 버튼으로 바뀌면 id 수정 하기
-        // TODO 2 : No 버튼 클릭 시 화면 전환 추가하기
-        val yesButton = findViewById<ImageButton>(R.id.restRoomButton) // O 버튼 id로 바꿔야 함
-        val noButton = findViewById<ImageButton>(R.id.wifiButton)   // X 버튼 id로 바꿔야 함
+        val yesButton = findViewById<LinearLayout>(R.id.yesButton)
+        val noButton = findViewById<LinearLayout>(R.id.noButton)
 
+        // TODO: 버튼 클릭 시 색상 변화 기능 및 아이콘 변경
         yesButton.setOnClickListener{
             val intent = Intent(this, QuestionActivity::class.java)
             intent.putExtra("layoutType", layoutType)
@@ -37,10 +50,19 @@ class OxSelectionAnswerActivity : AppCompatActivity() {
         }
 
         noButton.setOnClickListener{
-            val intent = Intent(this, HomeActivity::class.java)
-            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP)
-            startActivity(intent)
-            finish()
+            grpcClient.sendFastInquiry(title = layoutType, num = inquiryNumber) { success ->
+                runOnUiThread {
+                    if (success) {
+                        println("FastInquiry 성공")
+                    } else {
+                        println("FastInquiry 오류")
+                    }
+                    val intent = Intent(this, HomeActivity::class.java)
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP)
+                    startActivity(intent)
+                    finish()
+                }
+            }
         }
     }
 
